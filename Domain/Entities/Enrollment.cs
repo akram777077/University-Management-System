46a91ca032f6625e0cb4dfe46ca8d7ca807a34ec
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-04-24 18:07:39 +0100

    feat(domain): implement core entities

---
+++ Domain/Entities/Enrollment.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Entities
+{
+    public class Enrollment
+    {
+        public int StudentId { get; set; }
+        public int SectionId { get; set; }
+        public Section Section { get; set; } = null!;
+        public Student Student { get; set; } = null!;
+
+    }
+}
