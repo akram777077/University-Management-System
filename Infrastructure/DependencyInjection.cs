09ed685505757aa61f57640caf0682bf19206015
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-04-24 17:53:59 +0100

    feat(infra): add DependencyInjection for service registration

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+            //Register Infrastructure service here
+            //services.AddScoped<ICourseRepository, CourseRepository>();
+
+            return services;
+        }
+    }
+}
