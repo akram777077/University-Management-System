63557b6c666a45b71950a4bed3db72397ecdddef
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-04-24 17:55:56 +0100

    feat(app): add DependencyInjection for service registration

---
+++ Applications/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Applications
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            //Register Application service here
+            //services.AddScoped<ICourseService, CourseService>();
+
+            // Register cross-cutting concerns for the application layer
+            //services.AddAutoMapper();
+
+            return services;
+        }
+    }
+}
