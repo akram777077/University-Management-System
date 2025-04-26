76e25c94ef375ecdf49163cd5de8d2251a47ed75
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-04-26 10:34:04 +0100

    feat(app): add `IStudentService` interface
    - Created service layer contract for Student operations
    - Prepared for domain logic implementation

--- Applications/Interfaces/Services/IStudentService.cs
+++ Applications/Interfaces/Services/IStudentService.cs
@@ -1,7 +1,16 @@
-﻿namespace Applications.Interfaces.Repositories
+﻿using Applications.DTOs;
+using Domain.Entities;
+
+namespace Applications.Interfaces.Repositories
 {
     public interface IStudentService
     {
-
+        Task<IReadOnlyCollection<StudentDto>> GetStudentsAsync();
+        Task<StudentDto> GetByIdAsync(int id);
+        Task<StudentDto> GetByNameAsync(string name);
+        Task<int> AddStudent(Student student);
+        Task<StudentDto> UpdateAsync(int id, StudentDto studentDto);
+        Task<bool> DeleteAsync(int id);
+        Task<bool> DeleteByNameAsync(string name);
     }
 }
