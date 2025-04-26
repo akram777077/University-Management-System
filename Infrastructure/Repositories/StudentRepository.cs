94fd6eb6d50ab32b1ca5175fa5ac8de27e9b516e
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-04-26 10:44:56 +0100

    feat(infra): scaffold `StudentRepository`
    - Implemented IStudentRepository in the Infrastructure layer
    - Methods stubbed (throw NotImplementedException)
    - Prepared for EF Core/database implementation

---
+++ Infrastructure/Repositories/StudentRepository.cs
@@ -0,0 +1,43 @@
+﻿using Applications.Interfaces.Repositories;
+using Domain.Entities;
+
+namespace Infrastructure.Repositories
+{
+    public class StudentRepository : IStudentRepository
+    {
+        public Task<int> AddStudent(Student student)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Student> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Student> GetByNameAsync(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Student>> GetStudentsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Student> UpdateAsync(int id, Student student)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
