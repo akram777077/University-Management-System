f38e0547825f9e660fd6a7971bec7cadd857b511
akram <akram7.dris7@gmail.com>
2025-06-02 11:09:58 +0100

    feat(course): add and extend ICourseRepo interface with CRUD and query methods

--- Applications/Interfaces/Repositories/ICourseRepo.cs
+++ Applications/Interfaces/Repositories/ICourseRepo.cs
@@ -1,7 +1,17 @@
-﻿namespace Applications.Interfaces.Repositories
+﻿using Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Applications.Interfaces.Repositories
 {
     public interface ICourseRepo
     {
-
+        Task<IEnumerable<Course>> GetAllAsync();
+        Task<Course?> GetByIdAsync(int id);
+        Task AddAsync(Course course);
+        Task UpdateAsync(Course course);
+        Task DeleteAsync(int id);
+        Task<Course?> GetByNameAsync(string name);
+        Task<IEnumerable<Course>> GetByPriceRangeAsync(decimal minPrice, decimal maxPrice);
     }
 }
