d7c5ce9874c72639dc3f34a2fa7de0a569eb23c8
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-06-07 12:21:49 +0100

    feat(service): implement ICountryRepository interface with core query methods

---
+++ Applications/Interfaces/Repositories/ICountryRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Applications.Interfaces.Repositories
+{
+    public interface ICountryRepository
+    {
+        Task<Country?> GetByCodeAsync(string code);
+        Task<Country?> GetByIdAsync(int id);
+        Task<Country?> GetByNameAsync(string name);
+        Task<IEnumerable<Country>> GetListAsync();
+    }
+}
