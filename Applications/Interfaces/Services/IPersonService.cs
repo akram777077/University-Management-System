08bc22cd2fe954808b38974be9d74b4cefeb190f
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-06-07 14:17:59 +0100

    refactor(service): update IPersonService method signatures
    - Update AddAsync and UpdateAsync to accept nullable PersonRequest

--- Applications/Interfaces/Services/IPersonService.cs
+++ Applications/Interfaces/Services/IPersonService.cs
@@ -5,7 +5,7 @@ namespace Applications.Interfaces.Services
 {
     public interface IPersonService
     {
-        Task<Result<PersonResponse>> AddAsync(PersonRequest request);
+        Task<Result<PersonResponse>> AddAsync(PersonRequest? request);
         Task<Result> DeleteAsync(int id);
         Task<Result> DeleteAsync(string lastName);
         Task<Result> DoesExistAsync(int id);
@@ -13,6 +13,6 @@ namespace Applications.Interfaces.Services
         Task<Result<PersonResponse>> GetByIdAsync(int id);
         Task<Result<PersonResponse>> GetByNameAsync(string lastName);
         Task<Result<IReadOnlyCollection<PersonResponse>>> GetListAsync();
-        Task<Result> UpdateAsync(int id, PersonRequest request);
+        Task<Result> UpdateAsync(int id, PersonRequest? request);
     }
 }
