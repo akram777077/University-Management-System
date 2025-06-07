d7d51782fa72247d00c688253c1b4c2e6c3f4010
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-06-07 12:24:42 +0100

    feat(service): add CountryResponse DTO for API responses

---
+++ Applications/DTOs/CountryResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Applications.DTOs
+{
+    public class CountryResponse
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Code { get; set; }
+    }
+}
