4d62ad8f833bc0db3ab484a1119ca9948cf6f8d5
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-06-15 11:48:09 +0100

    feat(Presentation): Implement UsersController
    - Added a new UsersController to handle user-related API requests.
    - Implemented endpoints for GET, POST, PUT, DELETE operations on users.  Includes handling by ID and username.
    - Uses ResultExtension for consistent error handling.

---
+++ Presentation/Controllers/UsersController.cs
@@ -0,0 +1,162 @@
+﻿using Applications.DTOs.Users;
+using Applications.Helpers;
+using Applications.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Controllers.ResultExtension;
+
+namespace Presentation.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserService _service;
+
+        public UsersController(IUserService service)
+        {
+            _service = service;
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IEnumerable<UserResponse>>> GetList()
+        {
+            var result = await _service.GetListAsync();
+
+            if (!result.IsSuccess)
+                return result.HandleResult();
+
+            return Ok(result.Value);
+        }
+
+
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<UserResponse>> GetById(int id)
+        {
+            var result = await _service.GetByIdAsync(id);
+
+            if (!result.IsSuccess)
+                return result.HandleResult();
+
+            return Ok(result.Value);
+        }
+
+
+
+        [HttpGet("by-username/{username}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<UserResponse>> GetByUsername(string username)
+        {
+            var result = await _service.GetByUsernameAsync(username);
+
+            if (!result.IsSuccess)
+                return result.HandleResult();
+
+            return Ok(result.Value);
+        }
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<UserResponse>> Create(CreateUserRequest? request)
+        {
+            var response = await _service.AddAsync(request);
+
+            if (!response.IsSuccess)
+                return response.HandleResult();
+
+            return CreatedAtAction(nameof(GetById), new { id = response.Value.Id }, response.Value);
+        }
+
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var response = await _service.DeleteAsync(id);
+
+            if (!response.IsSuccess)
+                return response.HandleResult();
+
+            return NoContent();
+        }
+
+
+        [HttpDelete("by-username/{username}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> Delete(string username)
+        {
+            var response = await _service.DeleteAsync(username);
+
+            if (!response.IsSuccess)
+                return response.HandleResult();
+
+            return NoContent();
+        }
+
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> Update(int id, UpdateUserRequest? request)
+        {
+            var response = await _service.UpdateAsync(id, request);
+
+            if (!response.IsSuccess)
+                return response.HandleResult();
+
+            return NoContent();
+        }
+
+
+        [HttpPatch("{id}/role")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> UpdateUserRole(int id, UpdateUserRoleRequest? request)
+        {
+            var response = await _service.UpdateUserRoleAsync(id, request);
+
+            if (!response.IsSuccess)
+                return response.HandleResult();
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id}/password")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> ChangePassword(int id, ChangePasswordRequest? request)
+        {
+            var response = await _service.ChangePasswordAsync(id, request);
+
+            if (!response.IsSuccess)
+                return response.HandleResult();
+
+            return NoContent();
+        }
+    }
+}
