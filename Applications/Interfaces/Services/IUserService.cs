2e470603c920963f95cc598f169b58f5ce0f73f2
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-06-15 11:42:55 +0100

    feat(users): Add IUserService interface
    - Defines the IUserService interface for user management operations.
    - Includes methods for adding, updating, deleting, retrieving, and managing user roles.

---
+++ Applications/Interfaces/Services/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using Applications.DTOs.Users;
+using Applications.Shared;
+
+namespace Applications.Interfaces.Services
+{
+    public interface IUserService
+    {
+        Task<Result<UserResponse>> AddAsync(CreateUserRequest? request);
+        Task<Result> ChangePasswordAsync(int id, ChangePasswordRequest? request);
+        Task<Result> DeleteAsync(int id);
+        Task<Result> DeleteAsync(string username);
+        Task<bool> DoesExistAsync(int id);
+        Task<bool> DoesExistAsync(string username);
+        Task<Result<UserResponse>> GetByIdAsync(int id);
+        Task<Result<UserResponse>> GetByUsernameAsync(string username);
+        Task<Result<IReadOnlyCollection<UserResponse>>> GetListAsync();
+        Task<Result> UpdateAsync(int id, UpdateUserRequest? request);
+        Task<Result> UpdateUserRoleAsync(int id, UpdateUserRoleRequest? roleStatus);
+    }
+}
