4aa82d6e9c47ba4bdcaa36a85415f21b5471410c
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-06-28 20:18:02 +0100

    feat(Infrastructure): Add enrollment tracking improvements
    - Enhance database schema to improve enrollment tracking accuracy and efficiency.
    - Add new fields to the Enrollment entity to capture more detailed enrollment information.
    - Improve data integrity and reporting capabilities related to enrollments.

---
+++ Infrastructure/Migrations/20250628191159_ImproveEnrollmentTracking.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImproveEnrollmentTracking : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "graduation_date",
+                table: "enrollments",
+                newName: "actual_grad_date");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "enrollments",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "enrollments");
+
+            migrationBuilder.RenameColumn(
+                name: "actual_grad_date",
+                table: "enrollments",
+                newName: "graduation_date");
+        }
+    }
+}
