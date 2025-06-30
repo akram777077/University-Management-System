47aa8351da898b6caa53cf4339ca1c0bc3ea5d6a
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-06-30 08:49:50 +0100

    feat(Infrastructure): Implement CourseRepository using generic repository pattern
    - Improve code organization and reusability
    - Add GetByCodeAsync and DoesCodeExistAsync methods

---
+++ Infrastructure/Repositories/CourseRepository.cs
@@ -0,0 +1,23 @@
+using Applications.Interfaces.Repositories;
+using Domain.Entities;
+using Infrastructure.Data;
+using Infrastructure.Repositories.Base;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class CourseRepository(AppDbContext context) : GenericRepository<Course>(context), ICourseRepository
+{
+    // Additional methods specific to Program can be added here
+    public async Task<Course?> GetByCodeAsync(string code)
+    {
+        return await _context.Courses
+            .AsNoTracking()
+            .FirstOrDefaultAsync(c => c.Code == code);
+    }
+
+    public Task<bool> DoesCodeExistAsync(string code)
+    {
+        return _context.Courses.AnyAsync(c => c.Code == code);
+    }
+}
