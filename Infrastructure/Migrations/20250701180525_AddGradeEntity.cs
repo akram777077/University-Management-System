8b45dc38743c6b37698d7e4d1627c3eb65a78a8e
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-07-01 19:34:44 +0100

    feat(Infrastructure): Migrate Grade entity to the database
    - Added a new Grade entity to the database schema.
    - This entity will store grade information for students.
    - This is a necessary step for tracking student academic performance.

---
+++ Infrastructure/Migrations/20250701180525_AddGradeEntity.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddGradeEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "grades",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    score = table.Column<decimal>(type: "numeric(5,2)", nullable: false),
+                    date_recorded = table.Column<DateTime>(type: "timestamp with time zone", nullable: true, defaultValueSql: "CURRENT_TIMESTAMP"),
+                    comments = table.Column<string>(type: "varchar", maxLength: 200, nullable: true),
+                    student_id = table.Column<int>(type: "integer", nullable: false),
+                    course_id = table.Column<int>(type: "integer", nullable: false),
+                    semester_id = table.Column<int>(type: "integer", nullable: false),
+                    registration_id = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_grades", x => x.id);
+                    table.ForeignKey(
+                        name: "FK_grades_courses_course_id",
+                        column: x => x.course_id,
+                        principalTable: "courses",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_grades_registrations_registration_id",
+                        column: x => x.registration_id,
+                        principalTable: "registrations",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.SetNull);
+                    table.ForeignKey(
+                        name: "FK_grades_semesters_semester_id",
+                        column: x => x.semester_id,
+                        principalTable: "semesters",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_grades_students_student_id",
+                        column: x => x.student_id,
+                        principalTable: "students",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_grades_course_id",
+                table: "grades",
+                column: "course_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_grades_registration_id",
+                table: "grades",
+                column: "registration_id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_grades_semester_id",
+                table: "grades",
+                column: "semester_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_grades_student",
+                table: "grades",
+                column: "student_id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "grades");
+        }
+    }
+}
