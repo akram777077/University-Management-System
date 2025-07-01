32b3170c42cefe9de15a52f4f1a173d903611516
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-07-01 14:18:37 +0100

    refactor(Repositories): Add Course existence check by ID
    - Add DoesExistsAsync(int id) to ICourseRepository and CourseRepository
    - Allows checking for the existence of a course by its ID.
    - Improves efficiency by avoiding unnecessary database queries when checking for course existence.

--- Applications/Interfaces/Repositories/ICourseRepository.cs
+++ Applications/Interfaces/Repositories/ICourseRepository.cs
@@ -7,4 +7,5 @@ public interface ICourseRepository : IGenericRepository<Course>
 {
     Task<Course?> GetByCodeAsync(string code);
     Task<bool> DoesCodeExistAsync(string code);
+    Task<bool> DoesExistsAsync(int requestCourseId);
 }
