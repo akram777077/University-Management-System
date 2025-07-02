832cf9df9558795143de9db8d39634091d1c6db1
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-07-02 14:54:15 +0100

    refactor(Applications): Remove redundant application validation
    - Moved the validation to service class for simplicity.

--- Applications/Helpers/ServiceApplicationValidator.cs
+++ Applications/Helpers/ServiceApplicationValidator.cs
@@ -5,8 +5,5 @@ namespace Applications.Helpers;
 
 public static class ServiceApplicationValidator
 {
-    public static async Task<bool> ValidateNewApplication(this ServiceApplicationCreateRequest? request, IServiceApplicationRepository repository)
-    {
-        return await repository.DoesPersonHaveActiveApplicationsAsync(request.Value.PersonId, request.Value.ServiceOfferId);
-    }
+    
 }
