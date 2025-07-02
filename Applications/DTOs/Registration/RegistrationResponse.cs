9bdf75a7ec46bc87a33d348b8c621f98ff7ac341
Hocine-Bec <Bechebil.Houcin@gmail.com>
2025-07-02 08:56:27 +0100

    refactor(DTOs): Change StudentNumber and SemesterTermCode to string type
    - Changed the data type of StudentNumber and SemesterTermCode properties in RegistrationResponse DTO from integer to string.
    - Improves data integrity and flexibility in handling diverse student and semester code formats.

--- Applications/DTOs/Registration/RegistrationResponse.cs
+++ Applications/DTOs/Registration/RegistrationResponse.cs
@@ -5,8 +5,8 @@ public record struct RegistrationResponse
     public int Id { get; set; }
     public DateTime RegistrationDate { get; set; }
     public decimal RegistrationFees { get; set; }
-    public int StudentNumber { get; set; }
+    public string StudentNumber { get; set; }
     public string SectionNumber { get; set; }
-    public int SemesterTermCode { get; set; }
+    public string SemesterTermCode { get; set; }
     public int? ProcessedByUserId { get; set; }
 }
